e3ab42e85b42cd14661a480fabf6635fa1048c05
binyam <binyamwork@gmail.com>
2025-08-19 15:59:53 +0300

    f' commiting the task Implement the CategorySerializer and RecipeSerializer

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -3,16 +3,19 @@ from rest_framework import serializers
 from .models import Category, Recipe
 from django.contrib.auth.models import User
 
+#creatting a userserializer
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username']
 
+#creating a categoryserializer.
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
 
+#creating a RecipeSerializer
 class RecipeSerializer(serializers.ModelSerializer):
     owner = UserSerializer(read_only=True)
     category = CategorySerializer(read_only=True)
